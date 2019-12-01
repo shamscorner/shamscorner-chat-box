65cf8411bbe3182726e9bcfd8d545312f5192679
Shamim Hossain <hossains159@gmail.com>
2019-12-01 21:05:10 +0600

    add seeder for migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Message;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -23,22 +22,9 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'phone' => $faker->phoneNumber,
         'email' => $faker->unique()->safeEmail,
-        'profile_image' => 'http://via.placeholder.com/150x150',
+        'profile_image' => 'user.png',
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-    ];
-});
-
-$factory->define(Message::class, function (Faker $faker) {
-    do {
-        $from = rand(1, 15);
-        $to = rand(1, 15);
-    } while ($from === $to);
-
-    return [
-        'from' => $from,
-        'to' => $to,
-        'text' => $faker->sentence,
     ];
 });
